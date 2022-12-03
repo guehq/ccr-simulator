946656ec6bf85850268b03fbd8a08d7acf183dd1
Nevcan Uludas <nevcanuludas@gmail.com>
2022-12-03 20:05:42 +0400

    Fix, gas check

---
+++ js/gue-gas-dynamics.js
@@ -0,0 +1,35 @@
+/* ---------- GAS DYNAMICS ---------- */
+
+
+// NARCOSIS
+function checkNarcosis () {
+  ATA = depth.value / 10 + 1
+  fO2 = document.getElementById('fO2').value / 100
+  fHe = document.getElementById('fHe').value / 100
+  fN2 = 1 - fO2 - fHe
+  let narcoticGas = 1 - fHe
+
+  if (ATA * narcoticGas > 4) {
+    document.getElementById('gas_narcosis').classList.remove('is-hidden')
+  } else {
+    document.getElementById('gas_narcosis').classList.add('is-hidden')
+  }
+}
+
+// GAS DENSITY
+function checkDensity () {
+  ATA = depth.value / 10 + 1
+  fO2 = document.getElementById('fO2').value / 100
+  fHe = document.getElementById('fHe').value / 100
+  fN2 = 1 - fO2 - fHe
+  let ρO2 = 1.43 // rho
+  let ρN2 = 1.25
+  let ρHe = 0.18
+  let maxDensity = 5.2
+
+  if (ATA * ((fO2 * ρO2) + (fN2 * ρN2) + (fHe * ρHe)) > maxDensity) {
+    document.getElementById('gas_density').classList.remove('is-hidden')
+  } else {
+    document.getElementById('gas_density').classList.add('is-hidden')
+  }
+}
